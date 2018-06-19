ffa15192e93251eb031416e1511eb0ab25540c83
Vishal Kuo <vishalkuo@users.noreply.github.com>
2018-06-19 09:54:52 -0400

    adds support for migrations (#7)
    * adds support for migrations
    
    * Change pkey, use local_record_service db

--- record_service/record_service/models/user.py
+++ record_service/record_service/models/user.py
@@ -1,9 +1,10 @@
 from record_service.database.database import db
+from record_service.models.base import Base
 
 
-class User(db.Model):
+class User(Base):
     __tablename__ = "users"
 
     # This should probably be an email
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Text, primary_key=True)
     # Need password, metadata, other shit
