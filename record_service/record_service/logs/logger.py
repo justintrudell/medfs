d807a0c5d4063e04fb18a8c5d88a235bf6906187
Justin Trudell <justin.trudell.4@gmail.com>
2018-06-20 10:29:34 -0400

    remove yapf and comment on logger

--- record_service/record_service/logs/logger.py
+++ record_service/record_service/logs/logger.py
@@ -3,8 +3,9 @@ import logging
 import config
 
 
+# TODO: Implement more robust logging functionality
 def log(msg: str):
     if config.ENVIRONMENT == "DEV":
         print(msg)
     else:
-       logging.getLogger("record_service.root").info(msg)
+        logging.getLogger("record_service.root").info(msg)
