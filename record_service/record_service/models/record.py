ee1ae2b3f546d85387898f5852e6cfe61cb49348
Vishal Kuo <vishalkuo@users.noreply.github.com>
2018-06-19 22:39:45 -0400

    Adds fields for records and users db models (#8)
    * adds fields to record and user
    
    * change to use record_hash
    
    * Code review changes

--- record_service/record_service/models/record.py
+++ record_service/record_service/models/record.py
@@ -8,6 +8,6 @@ class Record(Base):
     __tablename__ = "records"
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    archived = db.column(db.Boolean)
-    hash = db.Column(db.Text)
-    # Need metadata, other shit
+    archived = db.Column(db.Boolean, nullable=False, default=False)
+    record_hash = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
--- record_service/record_service/models/user.py
+++ record_service/record_service/models/user.py
@@ -5,6 +5,5 @@ from record_service.models.base import Base
 class User(Base):
     __tablename__ = "users"
 
-    # This should probably be an email
     id = db.Column(db.Text, primary_key=True)
-    # Need password, metadata, other shit
+    hashed_password = db.Column(db.Text, nullable=False)
