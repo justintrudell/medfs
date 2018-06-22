0bdac789c0c7d210bfc49bbb2fc145297e52b8ef
Vishal Kuo <vishalkuo@users.noreply.github.com>
2018-06-21 20:53:00 -0400

    Adds support for ReactJs (#11)
    * Changes structure to support react
    
    * Removes unecessary changes

--- client/api/record_service.ts
+++ client/api/record_service.ts
@@ -4,9 +4,10 @@ import { resolve } from "url";
 
 export function get(endpoint: string, callback: Function) {
     const fullPath = resolve(constants.RECORD_SERVICE_ENDPOINT, endpoint);
+    // @ts-ignore: Requests typeshed has some issues around this
     request(fullPath.toString(), callback);
 }
 
-export function post(endpoint: string, data, callback: Function) {
+export function post(endpoint: string, data: any, callback: Function) {
     // TODO
 }
