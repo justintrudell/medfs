8aba64b39e6eb5950505b5436986d34132401b47
Jerry Wang <yisenjerrywang@gmail.com>
2018-06-22 13:50:08 -0400

    Add code to start service

--- acl/service/acl_server.py
+++ acl/service/acl_server.py
@@ -1,11 +1,15 @@
+from concurrent import futures
+import time
+
 import grpc
 
 import acl_pb2
 import acl_pb2_grpc
 
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
 # To be implemented
 class AclServicer(acl_pb2_grpc.AclServicer):
-
     def __init__(self):
         pass
 
@@ -26,3 +30,20 @@ class AclServicer(acl_pb2_grpc.AclServicer):
 
     def GetAllUsersForFile(self, request, context):
         pass
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    acl_pb2_grpc.add_AclServicer_to_server(AclServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+        print("Shutting down server")
+
+
+if __name__ == '__main__':
+    serve()
