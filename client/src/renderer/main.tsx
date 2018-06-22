d181b190f299c48f290f72ed8d9e860dea199865
Justin Trudell <justin.trudell.4@gmail.com>
2018-06-22 16:51:27 -0400

    undo client changes

--- client/src/renderer/main.tsx
+++ client/src/renderer/main.tsx
@@ -1,9 +1,17 @@
-import * as React from 'react';
+import * as React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./home";
+import Login from "./login";
+import Signup from "./signup";
 
 export default class extends React.Component {
-  render() {
-    return (
-      <p> hello </p>
-    );
-  }
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
+            </Switch>
+        );
+    }
 };
