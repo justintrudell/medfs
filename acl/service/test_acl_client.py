3b7c3785f291585bc5b32c1d19d729c3a722bdd4
Jerry Wang <yisenjerrywang@gmail.com>
2018-06-22 15:07:24 -0400

    Test acl client and real server stubs

---
+++ acl/service/test_acl_client.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+import grpc
+
+import acl_pb2
+import acl_pb2_grpc
+
+
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = acl_pb2_grpc.AclStub(channel)
+    response = stub.IsPermissionedForRead(acl_pb2.PermissionRequest(
+        user=acl_pb2.UserId(id=0), fileId="randomFile.txt")
+    )
+    print("Greeter client received: {}".format(response.result))
+
+    response = stub.GetAllFilesForUser(acl_pb2.GetFilesRequest(
+        requestor=acl_pb2.UserId(id=0), permission=acl_pb2.GetUsersRequest.READ)
+    )
+    print("Greeter client received: {}".format(response.files[0]))
+
+    response = stub.GetAllUsersForFile(acl_pb2.GetUsersRequest(
+        fileId="randomFile.txt", permission=acl_pb2.GetUsersRequest.READ)
+    )
+    print("Greeter client received: {}".format(response.users[0].id))
+
+
+if __name__ == '__main__':
+    run()
