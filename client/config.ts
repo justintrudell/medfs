ec31908295685ee84f6e1b46489bcfbd7c00ca6a
vishalkuo <vishalkuo@hotmail.com>
2018-06-21 20:02:06 -0400

    Changes structure to support react

--- client/config.ts
+++ client/config.ts
@@ -1,8 +1,11 @@
+import * as path from "path";
+
 const penv = process.env;
 
 const RECORD_SERVICE_HOST = penv.RECORD_SERVICE_HOST || "http://localhost";
 const RECORD_SERVICE_PORT = penv.RECORD_SERVICE_PORT || 3000;
 
 export const constants = {    
-    "RECORD_SERVICE_ENDPOINT":`${RECORD_SERVICE_HOST}:${RECORD_SERVICE_PORT}`
+    "RECORD_SERVICE_ENDPOINT":`${RECORD_SERVICE_HOST}:${RECORD_SERVICE_PORT}`,
+    "HOME_DIR": path.join(__dirname, "..")
 };
