63bf5e9a9307d86742619bed55635215bbec499d
vishalkuo <vishalkuo@hotmail.com>
2018-06-30 22:18:11 -0400

    Implements remember-me functionality to make decryption easier

--- client/src/api/record_service.ts
+++ client/src/api/record_service.ts
@@ -1,9 +1,12 @@
 import * as requestMaster from "request";
 import { constants } from "../config";
 import { resolve } from "url";
+const fileCookieStore = require("tough-cookie-filestore");
 
 export type RecordServiceResponse = Promise<requestMaster.Response>;
-const request = requestMaster.defaults({ jar: true });
+
+const jar = requestMaster.jar(new fileCookieStore(constants.COOKIE_STORAGE));
+const request = requestMaster.defaults({ jar });
 
 export function get(endpoint: string): RecordServiceResponse {
   return new Promise((pResolve, pReject) => {
