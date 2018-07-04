cb9f0ccbd851d65965e021d1b0713d5ecd0a6a2f
Dulwin <me@dulwin.com>
2018-07-04 09:02:32 -0400

    Fix schema (#31)
    * fix schema and endpoint
    
    * rebase onto master, run linter

--- record_service/record_service/models/record.py
+++ record_service/record_service/models/record.py
@@ -1,5 +1,6 @@
 from record_service.database.database import db
 from record_service.models.base import Base
+from record_service.models.user import User
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
@@ -8,6 +9,9 @@ class Record(Base):
     __tablename__ = "records"
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    creator_id = db.Column(UUID(as_uuid=True), db.ForeignKey(User.id), nullable=False)
+    acl_id = db.Column(UUID(as_uuid=True), nullable=False)
     archived = db.Column(db.Boolean, nullable=False, default=False)
     record_hash = db.Column(db.Text, nullable=False)
+    filename = db.Column(db.Text, nullable=False)
     created = db.Column(db.DateTime, nullable=False)
