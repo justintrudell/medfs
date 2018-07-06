a54d5b3748360ed84a006b26adf98fbeb1842bc3
Yisen Wang <y898wang@edu.uwaterloo.ca>
2018-07-06 15:07:16 -0400

    Add separate tables for permissions

--- acl/service/database/models/permission.py
+++ acl/service/database/models/permission.py
@@ -1,12 +1,11 @@
 from database.models.base import Base
 from sqlalchemy import Column, Boolean
 from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
 
 
-# Many to one relationship between permission and role
 class Permission(Base):
     __tablename__ = "permission"
 
-    user_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
-    record_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     is_readonly = Column(Boolean, nullable=False)
