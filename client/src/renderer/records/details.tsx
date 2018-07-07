96b1af30609c0e315029715dfa0d391ffe009860
Vishal Kuo <vishalkuo@users.noreply.github.com>
2018-07-07 11:58:57 -0400

    Record details (#47)
    * Adds record details
    
    * Removes ts-node

--- client/src/renderer/records/details.tsx
+++ client/src/renderer/records/details.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 import { get } from "../../api/records";
+import { RecordDetails } from "../../models/records";
+import * as _ from "lodash";
 
 interface MatchParams {
   record_id: string;
@@ -8,27 +10,50 @@ interface MatchParams {
 
 interface DetailProps extends RouteComponentProps<MatchParams> {}
 
-export class DetailView extends React.Component<DetailProps, {}> {
+interface DetailState {
+  recordDetails?: RecordDetails;
+}
+
+export class DetailView extends React.Component<DetailProps, DetailState> {
   constructor(props: DetailProps) {
     super(props);
+
+    this.state = {};
   }
 
-  componentWillMount() {
+  componentDidMount() {
     get(this.props.match.params.record_id)
       .then(result => {
-        console.log(result);
+        if (result.statusCode === 200) {
+          const recordDetails: RecordDetails = result.body.data;
+          this.setState({ recordDetails });
+        }
       })
       .catch(error => {
         console.log(error);
       });
   }
 
-  render() {
+  getToRender = (): JSX.Element => {
+    if (!this.state.recordDetails || _.isEmpty(this.state.recordDetails)) {
+      return <div />;
+    }
+    const pKeys = _.map(this.state.recordDetails, (value, key) => {
+      return (
+        <p key={key.toString()}>
+          {key}: {value.toString()}
+        </p>
+      );
+    });
     return (
       <div>
-        <p> {this.props.match.params.record_id} </p>
+        {pKeys}
         <button> Download </button>
       </div>
     );
+  };
+
+  render() {
+    return this.getToRender();
   }
 }
