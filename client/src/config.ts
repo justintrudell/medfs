3c7ead18949e09bac84279faff4f93d4d998aefa
Vishal Kuo <vishalkuo@users.noreply.github.com>
2018-07-11 10:31:32 -0400

    Download from ipfs (#51)
    * Adds ipfs
    
    * Adds download from ipfs functionality
    
    * Reverts tsconfig
    
    * Adds download indication

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,9 +5,11 @@ const penv = process.env;
 const RECORD_SERVICE_HOST = penv.RECORD_SERVICE_HOST || "http://localhost";
 const RECORD_SERVICE_PORT = penv.RECORD_SERVICE_PORT || 5000;
 const userDataPath = (app || remote.app).getPath("userData");
+const downloadPath = (app || remote.app).getPath("downloads");
 
 export const constants = {
   RECORD_SERVICE_ENDPOINT: `${RECORD_SERVICE_HOST}:${RECORD_SERVICE_PORT}`,
   LOGGEDIN_USER: "isLoggedIn",
-  COOKIE_STORAGE: join(userDataPath, "cookies.json")
+  COOKIE_STORAGE: join(userDataPath, "cookies.json"),
+  DOWNLOAD_PATH: downloadPath
 };
