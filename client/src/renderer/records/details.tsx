9b23e7da894ab19410cc2da8c690cab7b31596f4
vishalkuo <vishalkuo@hotmail.com>
2018-07-11 08:41:22 -0400

    Adds download indication

--- client/src/renderer/records/details.tsx
+++ client/src/renderer/records/details.tsx
@@ -7,6 +7,7 @@ import { ipfsNode } from "../../ipfs/ipfsProvider";
 import { writeFile } from "fs";
 import { constants } from "../../config";
 import { join } from "path";
+import { IPFSFile } from "ipfs";
 
 interface MatchParams {
   record_id: string;
@@ -16,13 +17,16 @@ interface DetailProps extends RouteComponentProps<MatchParams> {}
 
 interface DetailState {
   recordDetails?: RecordDetails;
+  downloadMessages: string[];
 }
 
 export class DetailView extends React.Component<DetailProps, DetailState> {
   constructor(props: DetailProps) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      downloadMessages: []
+    };
   }
 
   componentDidMount() {
@@ -47,22 +51,33 @@ export class DetailView extends React.Component<DetailProps, DetailState> {
       .get(this.state.recordDetails.hash)
       .then(result => {
         result.forEach(file => {
-          if (file.content) {
-            const filePath = join(
-              constants.DOWNLOAD_PATH,
-              this.state.recordDetails!.filename
-            );
-            writeFile(filePath, file.content, err => {
-              if (err) {
-                console.log(err);
-              }
-            });
-          }
+          this.downloadFile(file);
         });
       })
-      .catch(error => {
-        console.log("error", error);
+      .catch(err => {
+        this.setState({ downloadMessages: [err.message] });
       });
+  };
+
+  downloadFile = (file: IPFSFile): void => {
+    if (file.content) {
+      const filePath = join(
+        constants.DOWNLOAD_PATH,
+        this.state.recordDetails!.filename
+      );
+      writeFile(filePath, file.content, err => {
+        if (err) {
+          this.setState({ downloadMessages: [err.message] });
+        } else {
+          this.setState(prevState => ({
+            downloadMessages: [
+              ...prevState.downloadMessages,
+              `Downloaded to ${filePath}`
+            ]
+          }));
+        }
+      });
+    }
   };
 
   getToRender = (): JSX.Element => {
@@ -80,6 +95,11 @@ export class DetailView extends React.Component<DetailProps, DetailState> {
       <div>
         {pKeys}
         <button onClick={this.downloadRecord}> Download </button>
+        <ul>
+          {this.state.downloadMessages.map((message, idx) => {
+            return <li key={idx}> {message} </li>;
+          })}
+        </ul>
       </div>
     );
   };
