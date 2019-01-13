3b155c79a44dc89efd0b9172849677c182f81e50
Dulwin Jayalath <dejayala@edu.uwaterloo.ca>
2019-01-13 14:32:17 -0500

    refactor permissions code to make change permissions endpoints easier

--- record_service/record_service/endpoints/record_api.py
+++ record_service/record_service/endpoints/record_api.py
@@ -1,28 +1,25 @@
 import json
-import os
 from typing import Any, Dict, List
 
 from flask import Blueprint, request
 from flask_login import login_required, current_user
-from werkzeug import secure_filename, ImmutableMultiDict
+from werkzeug import ImmutableMultiDict
 
 import config
 from record_service.database.database import db
 from record_service.external import acl_api, queueing_api
 from record_service.models.record import Record
-from record_service.models.user import User
-from record_service.utils.exceptions import (
-    PermissionModificationError,
-    UserNotFoundError,
-)
+
 from record_service.utils.responses import JsonResponse
 from record_service.utils.file_uploader import FileUploader, IpfsWriter
+from record_service.utils.permissions import (
+    parse_uploaded_permissions,
+    create_acl_permissions,
+)
 
 
 record_api = Blueprint("record_api", __name__)
 UPLOADER = FileUploader(IpfsWriter())
-PermissionsDict = Dict[str, Dict[str, str]]
-UploadRequest = List[PermissionsDict]
 
 
 # Data object:
@@ -143,15 +140,18 @@ def upload_file():
     if not valid:
         return msg, 400
 
-    perms_with_uuid = _parse_permissions(permissions_json)
-    file_path = _save_uploaded_file(request.files["file"])
+    perms_with_uuid = parse_uploaded_permissions(permissions_json, db)
 
     # Upload the file to IPFS
-    new_record = UPLOADER.upload(data["filename"], file_path, data["extension"])
+    new_record = UPLOADER.upload(
+        request.files["file"],
+        data["filename"],
+        data["extension"],
+    )
     new_record.creator_id = current_user.get_id()
 
     # Update permissions in the ACL service
-    _create_acl_permissions(str(new_record.id), perms_with_uuid)
+    create_acl_permissions(current_user.get_id(), str(new_record.id), perms_with_uuid)
 
     # Push out keys to message service
     for user_uuid, values in perms_with_uuid.items():
@@ -169,42 +169,3 @@ def upload_file():
     db.session.commit()
 
     return str(new_record.id), 200
-
-
-def _parse_permissions(permissions_json: UploadRequest) -> PermissionsDict:
-    permissions_dict = {dct["email"]: dct["values"] for dct in permissions_json}
-    perms_with_uuid = {}
-    for email, values in permissions_dict.items():
-        user_obj = db.session.query(User).filter_by(email=email).one_or_none()
-        if user_obj is None:
-            raise UserNotFoundError(f"User {email} did not exist")
-        perms_with_uuid[str(user_obj.id)] = values
-    return perms_with_uuid
-
-
-def _save_uploaded_file(flask_file) -> str:
-    if not flask_file.filename:
-        raise ValueError("No filename on uploaded file")
-    # TODO: add header data to file
-    filename = secure_filename(flask_file.filename)
-    path = os.path.join("/tmp", filename)
-    flask_file.save(path)
-    return path
-
-
-def _create_acl_permissions(record_uuid: str, permissions_dict: PermissionsDict):
-    acl_client = acl_api.build_client(config.ACL_URL, config.ACL_PORT)
-    user_id = current_user.get_id()
-    ret = acl_api.add_record(acl_client, user_id, record_uuid)
-    if not ret.result:
-        raise PermissionModificationError("Failed to add record")
-
-    permissions = {key: dct["permission"] for key, dct in permissions_dict.items()}
-    # Since _create_acl_permissions needs a "state of the world",
-    # we have to add ourselves to this state of the world
-    permissions[user_id] = "WRITE"
-    ret = acl_api.set_permissions(
-        acl_client, current_user.get_id(), record_uuid, permissions
-    )
-    if not ret.result:
-        raise PermissionModificationError("Failed to modify permissions on record")
