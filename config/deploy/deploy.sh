b0c580191c654f0287f4b8ff00f40cf0b562f38c
Justin Trudell <justin.trudell.4@gmail.com>
2019-02-02 22:55:46 -0500

    Split deploys for rec and msg srv (#114)

--- config/deploy/deploy.sh
+++ config/deploy/deploy.sh
@@ -2,11 +2,20 @@
 
 cd config/deploy/
 
-echo "Composing"
-ecs-cli compose --project-name medfs service up \
+echo "Composing core"
+ecs-cli compose -f docker-compose.core.yml --project-name medfs-core service up \
+   --cluster-config default \
+   --launch-type FARGATE \
+   # Uncomment the lines below if you need to recreate the service. Otherwise ecs-cli throws a warning
+   # --container-name record_service \
+   # --container-port 5000 \
+   # --target-group-arn arn:aws:elasticloadbalancing:us-east-1:361641763854:targetgroup/medfs-core/0b6fc21eb62b8100 \ 
+
+echo "Composing message service"
+ecs-cli compose -f docker-compose.message-srv.yml --project-name medfs-message service up \
     --cluster-config default \
     --launch-type FARGATE \
     # Uncomment the lines below if you need to recreate the service. Otherwise ecs-cli throws a warning
-    # --container-name record_service \
-    # --container-port 5000 \
-    # --target-group-arn arn:aws:elasticloadbalancing:us-east-1:361641763854:targetgroup/medfs/70ace18547f60eb7 \
+    # --container-name message_service \
+    # --container-port 5004 \
+    # --target-group-arn arn:aws:elasticloadbalancing:us-east-1:361641763854:targetgroup/medfs-message/06bc20ff6087f77b \
