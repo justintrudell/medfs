dd3fcf2bc68be544ad1fed6746ac9461f67aff40
Justin Trudell <justin.trudell.4@gmail.com>
2019-02-25 08:25:49 -0500

    Implement client ipfs connection (#130)

--- client/src/ipfs/ipfsProvider.ts
+++ client/src/ipfs/ipfsProvider.ts
@@ -1,8 +1,8 @@
 import * as IPFS from "ipfs";
-import { constants } from "../config";
-import { exec } from "child_process";
 
 const ipfsNode = new IPFS({});
+const remoteNode =
+  "/ip4/54.145.221.117/tcp/4001/ipfs/QmfPsMDnvfPRh7xXYfbUMHbT7TZLor5M7iHB29E1ouhz7R";
 
 let ipfsPromise: Promise<IPFS>;
 export function getIpfs(): Promise<IPFS> {
@@ -12,24 +12,16 @@ export function getIpfs(): Promise<IPFS> {
 
   ipfsPromise = new Promise((resolve, reject) => {
     ipfsNode.on("ready", () => {
-      if (constants.IS_PROD) {
-        resolve(ipfsNode);
-        return;
-      }
-
-      // TODO: are there issues with using this path? Also this probably isn't windows friendly
-      exec("src/scripts/ipfsAddr.sh", (_error, stdout: string, _stderr) => {
-        ipfsNode.swarm
-          .connect(stdout)
-          .then(() => {
-            resolve(ipfsNode);
-            return;
-          })
-          .catch(err => {
-            reject(err);
-            return;
-          });
-      });
+      ipfsNode.swarm
+        .connect(remoteNode)
+        .then(() => {
+          resolve(ipfsNode);
+          return;
+        })
+        .catch(err => {
+          reject(err);
+          return;
+        });
     });
   });
 
